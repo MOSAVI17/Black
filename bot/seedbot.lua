1bbe27abbc7fb726a02a5314c30faf0cd064717f
MOSAVI17 <www.mosavi7857@gmail.com>
2016-01-26 18:25:35 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -221,19 +221,16 @@ function create_config( )
     "all",
     "leave_ban"
     },
-    sudo_users = {110626080,103649648,111020322,0,tonumber(our_id)},--Sudo users
+    sudo_users = {171400558,tonumber(our_id)},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
 An advance Administration bot based on yagop/telegram-bot 
 
-https://github.com/SEEDTEAM/TeleSeed
 
-Admins
-@iwals [Founder]
-@imandaneshi [Developer]
-@Rondoozle [Developer]
-@seyedan25 [Manager]
+
+Admin
+@Black_Hat_admin1
 
 Special thanks to
 awkward_potato
@@ -242,8 +239,7 @@ topkecleon
 Vamptacus
 
 Our channels
-@teleseedch [English]
-@iranseed [persian]
+@Freebot1
 ]],
     help_text_realm = [[
 Realm Commands:
